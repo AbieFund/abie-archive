c0fb6aed5957e0f640d0e4a072be810fa8cd2b83
Julien Béranger <jb@abie.fund>
2016-03-18 15:23:58 +0100

    corrected a few things

--- abie.sol
+++ abie.sol
@@ -1,22 +1,42 @@
+// The contract hasn't even been tested yet.
+//
+// The expected behaviour is the following : 
+//
+// (1) If there's enough money on the contract --> send the amount to the corresponding address (array's first line)
+//
+// (2) When a project is voted (data from the vote contract) --> add one line in the array
+//
+// With donations, it makes 3 different types of state change independant from each others
+
 contract Abie {
   
+  // Donors send money to the contract
   struct Donor {
     address donorAddress;
     uint donorAmount;
   }
-    
-  // Get recipient's address and amount from the vote contract (1 address and 1 uint)
-    
-  // Put these 2 values at the end of an array
+  
+  // We get 2 values from the vote contract
+  struct votedProject {
+    address votedAddress;
+    uint votedAmount;
+  }
+  
+  // Create an array
+  address[] array;
+  mapping (address => uint) amountNeeded;
 
-  // Take the first line and declare these 2 variables : addr and amount
+  // Extract the 2 values from the array's 1st line
+  amount = amountNeeded[0];
+  addr = address[0];
     
-  // as long as there's money on the contract, send amount to the first recipient on the list
+  // If balance is enough, 
   if (balance >= amount){
         
+    // send it to the corresponding address    
     addr.send(amount);
             
-    // delete the element and move everything down an index 
+    // delete the 1st line and down an index
     function remove(uint index)  returns(uint[]) {
         if (index >= array.length) return;
 
@@ -29,5 +49,4 @@ contract Abie {
     }
   }
 }
-    
     
